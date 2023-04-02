2b2a96da6c87f0dea32b1dfc6e0eda0017ab45aa
Sanyo <66254994+ashiksanyo10@users.noreply.github.com>
2023-04-02 05:11:44 -0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,17 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <p>
-      Sometimes I tweet 
-        <a href="https://twitter.com/AshSanyo" target="blank"> <u>here</u></a>
-        , 
-        
-        this is my favourite
-        <a href="https://giphy.com/gifs/Friends-season-6-the-one-with-unagi-episode-617-JOe7JxOiMg61ogl6fH/fullscreen" target="blank"> <u>gif</u></a>  
-        ,
-
-        here is my favourite
-        <a href="https://twitter.com/ScienceMemes6/status/1362504877889454081?s=20&t=lIiaaUethpTNNdQVtYe60Q" target="blank"> <u>tweet</u></a> 
-      
+       ✔️ I'm not a robot!
       ,
 
       And this is my instagram handle
